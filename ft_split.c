b2426d934f1351ff81f4c26f86d122bf9bf82ea0
ghedaamagableh <magablehghedaa@gmail.com>
2025-12-07 22:00:11 +0300

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -1,87 +1,95 @@
 #include "libft.h"
 
-static int count_word(const char *s, char c)
+static char	**free_array(char **ptr, int i)
 {
-	int count;
-	count  = 0;
-
-	while(*s)
+	while (i > 0)
 	{
-		(*s == c)
-			s++;
-		if(*s != c && *s != '\0')
+		i--;
+		free(ptr[i]);
+	}
+	free(ptr);
+	return (0);
+}
+
+static int	ft_count_words(char const *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			i++;
+		else
 		{
 			count++;
-		while(*s != c %% *s != '\0)
-			s++;
+			while (str[i] && str[i] != c)
+				i++;
 		}
-		return (count);
 	}
+	return (count);
 }
 
-static int word_len(const char *s, char c)
+static char	*ft_putword(char *word, char const *s, int i, int word_len)
 {
-	int len;
-	len = 0;
-	while (s[len] != c && s[len] != '\0)
-		len++;
-	return len;
-}
+	int	j;
 
-static char save_word(const char *s, int len)
-{
-	char *word;
-	int i;
-
-	word = malloc(len +1);
-	i = 0;
-	if(!word)
-		return NULL;
-	while (i < len)
+	j = 0;
+	while (word_len > 0)
 	{
-		word[i] = s[i];
-		i++;
+		word[j] = s[i - word_len];
+		j++;
+		word_len--;
 	}
-	word[i] = '\0';
+	word[j] = '\0';
 	return (word);
 }
-char **ft_split(char const *s, char c)
+
+static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
 {
-	char **aray;
-	int word ;
-	int i;
-	int len;
-	if(!s)
-		return (NULL);
-	word = word_len(s,c);
-	aray = malloc((word + 1) * sizeof(char*));
-	while (*s)
+	int	i;
+	int	word;
+	int	word_len;
+
+	i = 0;
+	word = 0;
+	word_len = 0;
+	while (word < num_words)
 	{
-		while(*s == c)
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
 		{
-			s++;
-			if (*s != c && *s != '\0')
-			{
-				len= word_len(s, c);
-			       array[i] = save_word(s, len);
-		       if(!array)
-		       {
-				while(i >  0)
-			 		free(aray[i--]);
-				free(aray);
-				return NULL;
-			}
-       			i++;
- 			s +=len
-			}
+			i++;
+			word_len++;
 		}
-	array[i] = NULL;
-	return array;
+		s2[word] = (char *)malloc(sizeof(char) * (word_len + 1));
+		if (!s2)
+			return (free_array(s2, word));
+		ft_putword(s2[word], s, i, word_len);
+		word_len = 0;
+		word++;
 	}
-}	
+	s2[word] = 0;
+	return (s2);
+}
 
+char	**ft_split(char const *s, char c)
+{
+	char			**s2;
+	unsigned int	num_words;
 
-
+	if (!s)
+		return (0);
+	num_words = ft_count_words(s, c);
+	s2 = (char **)malloc(sizeof(char *) * (num_words + 1));
+	if (!s2)
+		return (0);
+	s2 = ft_split_words(s, c, s2, num_words);
+	return (s2);
+}
 
 
 
